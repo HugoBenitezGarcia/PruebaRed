33340276a4cc6633afa7799fb61e5ca90e91fb43
HugoBenitezGarcia <hbenagr0801@iesrafaelalberti.es>
2026-01-26 10:06:44 +0100

    Primer commit

---
+++ Practica_Broadcast.py
@@ -0,0 +1,22 @@
+# BUSCANDO, Puerto 4000, id aleatorio 0-9999 (el id mas alto es el host)
+
+import socket
+import random
+
+def host():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    while
+
+host()
+
+def cliente():
+
+
+
+cliente()
+
+def BuscarPartida():
+    #Aqui va buscar la ip
+BuscarPartida()
