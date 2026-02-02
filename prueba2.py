67d4de746566ea4a8e3296b58b9d2b48ffd080f4
HugoBenitezGarcia <hbengar0801@g.educaand.es>
2026-02-02 08:43:53 +0100

    actu

--- prueba2.py
+++ prueba2.py
@@ -32,7 +32,7 @@ def BuscarPartida():
     #si no responde nadie en 5 segundos se vuelve a alanzar la peticion desde el except 
     sock.settimeout(1)
 
-    mensaje = f"DESCUBIR;{ID};{nombre}".encode()
+    mensaje = f"DESCUBRIR;{ID};{nombre}".encode()
 
     #Mandar broadcast
     sock.sendto(mensaje, (dir_broadcast, puerto))
@@ -40,30 +40,31 @@ def BuscarPartida():
 
     #Bucle para encontrar la otra maquina
     while not emparejado:
-        try:
-            data, addr = sock.recvfrom(1024) #Aqui se recibe los datos de la otra maquina
-            texto = data.decode() #Pasar de bytes a string
-            tipo, id_oponente, nombre = texto.split(";")
-            id_oponente = int(id_oponente)
-            
-            if id_oponente != ID:
-                continue
-
-            print("Partida encontrada")
-            emparejado = True #se cierra el emparejamiento
-
-            #Aqui se decide quien es host y cliente 
-            #if ID > id_oponente:  #si sale un nuemro mayor eres host 
-                #host(addr[0])
-            #else:  #si sale menor eres cliente
-                #cliente(addr[0])
-
-            sock.close() #Cierro socket para que no busque mas maquinas 
-
-        #si se acaba el tiempo de 5 segundos vuelve a buscar oponente
-        except socket.timeout:
-            print('No se encuentran jugadores')
-            sock.sendto(mensaje, (dir_broadcast, puerto))
+    try:
+        data, addr = sock.recvfrom(1024)
+        texto = data.decode()
+        partes = texto.split(";")
+        if len(partes) != 3:
+            continue
+        
+        tipo, id_oponente, nombre = partes
+        
+        if tipo != "DESCUBRIR":  # Ignorar mensajes que no sean DESCUBRIR
+            continue
+        
+        id_oponente = int(id_oponente)
+        
+        if ID == id_oponente:  # Ignorar mi propio broadcast
+            continue
+        
+        print(f"Partida encontrada con {nombre}")
+        emparejado = True
+        
+        sock.close()
+        
+    except socket.timeout:
+        print('No se encuentran jugadores')
+        sock.sendto(mensaje, (dir_broadcast, puerto))
 BuscarPartida()
 
 # HACER PING AL SERVIDOR DE GOOGLE Y QUE ME DEVUELVA MI IP, CALCULAR EL BROADCAST DE LA  SUBRED CON EL /24 
